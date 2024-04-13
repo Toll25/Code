e99f9de59138075a7b6f77ffc0e2e8eb485975a3
Toll <paulipanther.hoesch@gmail.com>
2024-04-13 17:32:08 +0200

    update

--- Rust/data_generator/src/main.rs
+++ Rust/data_generator/src/main.rs
@@ -13,6 +13,7 @@ use rayon::prelude::*;
 use serde::Serialize;
 use std::fs::File;
 use std::io::Write;
+use std::time::Instant;
 use toml;
 
 #[derive(Dummy, Serialize, Clone)]
@@ -49,25 +50,47 @@ async fn send_to_postgres(
     database_url: &str,
     table_name: &str,
 ) -> Result<(), tokio_postgres::Error> {
-    // Connect to PostgreSQL
-    let (client, connection) = tokio_postgres::connect(database_url, tokio_postgres::NoTls).await?;
+    let (mut client, connection) =
+        tokio_postgres::connect(database_url, tokio_postgres::NoTls).await?;
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
         }
     });
 
-    // Prepare the statement
-    let statement = client
+    let check_table_exists_query = format!(
+        "CREATE TABLE IF NOT EXISTS {} (
+            id SERIAL PRIMARY KEY,
+            firstname VARCHAR(255),
+            lastname VARCHAR(255),
+            username VARCHAR(255),
+            city VARCHAR(255),
+            age INTEGER,
+            email VARCHAR(255),
+            phone_number VARCHAR(255)
+        )",
+        table_name
+    );
+    client.execute(&check_table_exists_query, &[]).await?;
+
+    let transaction = client.transaction().await?;
+
+    let progress_bar = ProgressBar::new(users.len() as u64);
+    progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] [{bar:40}] {pos}/{len} PostgreSQL insertions ({eta})")
+            .progress_chars("=> "),
+    );
+
+    let statement = transaction
         .prepare(&format!(
             "INSERT INTO {} (firstname, lastname, username, city, age, email, phone_number) VALUES ($1, $2, $3, $4, $5, $6, $7)",
             table_name
         ))
         .await?;
 
-    // Execute the statement for each user
     for user in users {
-        client
+        transaction
             .execute(
                 &statement,
                 &[
@@ -75,13 +98,19 @@ async fn send_to_postgres(
                     &user.lastname,
                     &user.username,
                     &user.city,
-                    &(user.age as i32), // PostgreSQL expects age as an integer
+                    &(user.age as i32),
                     &user.email,
                     &user.phone_number,
                 ],
             )
             .await?;
+
+        progress_bar.inc(1);
     }
+
+    transaction.commit().await?;
+
+    progress_bar.finish_with_message("Insertions completed");
 
     Ok(())
 }
@@ -90,27 +119,41 @@ async fn send_to_mongodb(
     users: Vec<User>,
     database_name: &str,
     collection_name: &str,
+    use_insert_many: bool,
 ) -> Result<(), mongodb::error::Error> {
-    // Connect to MongoDB
     let client = Client::with_uri_str("mongodb://localhost:27017/").await?;
     let db: Database = client.database(database_name);
     let collection: Collection<Document> = db.collection(collection_name);
 
-    // Convert users to BSON documents and insert into collection
-    let user_documents: Vec<Document> = users
-        .into_iter()
-        .map(|user| bson::to_document(&user).unwrap())
-        .collect();
+    if !use_insert_many {
+        let progress_bar = ProgressBar::new(users.len() as u64);
+        progress_bar.set_style(
+            ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] [{bar:40}] {pos}/{len} MongoDB insertions ({eta})")
+                .progress_chars("=> "),
+        );
 
-    // Insert documents into collection
-    collection.insert_many(user_documents, None).await?;
+        for user in users {
+            let user_document = bson::to_document(&user).unwrap();
+            collection.insert_one(user_document, None).await?;
+            progress_bar.inc(1);
+        }
+
+        progress_bar.finish_with_message("Insertions completed");
+    } else {
+        let user_documents: Vec<Document> = users
+            .into_iter()
+            .map(|user| bson::to_document(&user).unwrap())
+            .collect();
+
+        collection.insert_many(user_documents, None).await?;
+    }
 
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    // Prompt the user for the number of datasets to generate
     let num_datasets: u64 = loop {
         let input: String = Input::with_theme(&ColorfulTheme::default())
             .with_prompt("Enter the number of datasets to generate (multiples of 10000, default: 1 million):")
@@ -133,7 +176,8 @@ async fn main() {
 
     let progress_bar = ProgressBar::new(num_chunks);
     progress_bar.set_style(
-        ProgressStyle::default_bar().template("[{elapsed_precise}] [{bar:40}] {pos}/{len} chunks"),
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] [{bar:40}] {pos}/{len} chunks generated ({eta})"),
     );
 
     let users: Vec<User> = (0..num_chunks)
@@ -174,14 +218,25 @@ async fn main() {
         Some(choice) => {
             let send_to_mongodb_bool = choice == 0 || choice == 2;
             let send_to_postgres_bool = choice == 1 || choice == 2;
+            let mut use_insert_many = true;
 
-            // If user chooses to send to MongoDB
             if send_to_mongodb_bool {
-                match send_to_mongodb(users.clone(), "your_database_name", "your_collection_name")
-                    .await
-                {
+                use_insert_many = Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Do you want to take advantage of MongoDB insert_many? (gives MongoDB an unfair advantage)")
+                    .default(true)
+                    .interact()
+                    .unwrap_or_else(|e| {
+                        eprintln!("Failed to interact with user: {}", e);
+                        std::process::exit(1);
+                    });
+            }
+
+            if send_to_mongodb_bool {
+                let start = Instant::now();
+                match send_to_mongodb(users.clone(), "data", "users", use_insert_many).await {
                     Ok(_) => {
-                        println!("Data sent to MongoDB successfully");
+                        let duration = start.elapsed();
+                        println!("Data sent to MongoDB successfully in {:?}", duration);
                     }
                     Err(err) => {
                         eprintln!("Failed to send data to MongoDB: {}", err);
@@ -190,14 +245,17 @@ async fn main() {
             }
 
             if send_to_postgres_bool {
-                if let Err(err) = send_to_postgres(
-                    users,
-                    "postgresql://username:password@localhost/database",
-                    "users",
-                )
-                .await
+                let start = Instant::now();
+                match send_to_postgres(users, "postgresql://user:password@localhost/data", "users")
+                    .await
                 {
-                    eprintln!("Failed to send data to PostgreSQL: {}", err);
+                    Ok(_) => {
+                        let duration = start.elapsed();
+                        println!("Data sent to PostgreSQL successfully in {:?}", duration);
+                    }
+                    Err(err) => {
+                        eprintln!("Failed to send data to PostgreSQL: {}", err);
+                    }
                 }
             }
         }
